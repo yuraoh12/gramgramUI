5881cb6efad96cba6179f3c3da33f457eef5052d
OH YURA <102283560+yuraoh12@users.noreply.github.com>
2023-05-01 22:13:32 +0900

    Add files via upload

---
+++ simpleDb/Sql.java
@@ -0,0 +1,28 @@
+package com.ll.simpleDb;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Sql {
+    private final SimpleDb simpleDb;
+    private final StringBuilder sqlBuilder;
+    private final List<Object> params;
+
+    public Sql(SimpleDb simpleDb) {
+        this.simpleDb = simpleDb;
+        this.sqlBuilder = new StringBuilder();
+        this.params = new ArrayList<>();
+    }
+
+    public Sql append(String sqlPart, Object... args) {
+        sqlBuilder.append(sqlPart).append(" ");
+        Collections.addAll(params, args);
+        return this;
+    }
+
+    public long insert() {
+        String sql = sqlBuilder.toString();
+        return simpleDb.runInsertAndGetGeneratedKey(sql, params.toArray());
+    }
+}
